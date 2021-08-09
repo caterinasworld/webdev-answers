722c924b5e578e550446deb60a2c5e8528a2a5ca
Caterina Paun <caterina.paun@gmail.com>
2021-08-09 00:07:42 -0700

    Add: solutions to homework 5

---
+++ hw5/03-beverages.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+  const url =
+    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+
+  const addElementToDOM = (element) => {
+    $('#results')
+      .css({
+        display: 'flex',
+        flexFlow: 'row wrap',
+        justifyContent: 'center',
+        marginTop: '2rem',
+      })
+      .append(
+        $('<figure>')
+          .css({
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginTop: '20px',
+          })
+          .append(
+            $('<img>')
+              .css('width', '100%')
+              .attr({
+                src: `${element.strDrinkThumb}`,
+                alt: `${element.strDrink}`,
+              })
+          )
+          .append($('<figcaption>').append($('<h4>').text(element.strDrink)))
+      );
+  };
+
+  const fetchData = (url) => {
+    $.ajax({
+      type: 'GET',
+      url: url,
+      success: (data) => {
+        console.log(data);
+
+        data.drinks.forEach((item) => {
+          addElementToDOM(item);
+        });
+      },
+      error: (error) => {
+        console.log(error);
+        $('#results')
+          .append('<div')
+          .css('marginBottom', '2rem')
+          .text(`An error occured. Please try again.`);
+      },
+      complete: () => {
+        $('#loading').remove();
+      },
+    });
+  };
+
+  fetchData(url);
+});
